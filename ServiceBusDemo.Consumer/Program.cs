b3a493fb74a8d5d25bf82c98f8795d7cc1506783
Simon Timms <stimms@gmail.com>
2014-02-20 06:51:22 -0700

    initial commit

---
+++ ServiceBusDemo.Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.WindowsAzure;
+using System.Collections.Generic;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusDemo.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("ServiceBusConnection");
+
+            QueueClient client = QueueClient.CreateFromConnectionString(connectionString, "imports");
+
+            while (true)
+            {
+                var message = client.Receive();
+                if (message != null)
+                {
+                    try
+                    {
+                        MainExtracted(message);
+
+                        message.Complete();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine(ex);
+                        message.Abandon();
+                    }
+                }
+
+            }
+        }
+        private static void MainExtracted(BrokeredMessage message)
+        {
+            Console.WriteLine("Body:" + message.GetBody<string>());
+            Console.WriteLine("Properties:");
+            foreach (var property in message.Properties)
+            {
+                Console.WriteLine(String.Format("\t{0}: {1}", property.Key, property.Value));
+            }
+        }
+    }
+}
---
+++ ServiceBusDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure;
+using System.Collections.Generic;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMessage()
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("ServiceBusConnection");
+
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "imports");
+
+            var message = new BrokeredMessage("Hello world " + DateTime.Now); 
+            message.Properties.Add("from", "Simon");
+            message.Properties.Add("platform", Request.Browser.Platform);
+            
+            Client.Send(message);
+            
+            ViewBag.Message = "Message sent.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
